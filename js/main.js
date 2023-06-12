2f7d858cd6dc3b4ef2d9b4f97df7a6ba30e24849
marcocampisi <marco.campisi@outlook.it>
2023-06-12 10:23:46 +0200

    Structure fix

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ const myP = document.getElementById('my-p');
 let userFirstName = prompt("Inserisci il tuo nome");
 let userLastName = prompt("Inserisci il tuo cognome");
 let userFavouriteColor = prompt("Inserisci il tuo colore preferito");
+let userPassword = userFirstName + userLastName + userFavouriteColor + 101;
 
-myP.innerHTML = userFirstName + userLastName + userFavouriteColor + 101;
+myP.innerHTML = userPassword;
